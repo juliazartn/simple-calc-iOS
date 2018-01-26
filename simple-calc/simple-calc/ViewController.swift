0b1f23c558aee25b6aa9b2e91b187fb17fbecf86
juliazartn <julia.zartn@gmail.com>
2018-01-26 11:09:43 -0800

    initial set up commit

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Julia Zaratan on 1/26/18.
+//  Copyright © 2018 Julia Zaratan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
